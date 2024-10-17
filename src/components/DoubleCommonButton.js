965ac05c3b446616a407b00ae4b742159fd3d2ab
ADMIN <21522704@gm.uit.edu.vn>
2024-10-17 09:09:58 +0700

    add component button

---
+++ src/components/DoubleCommonButton.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { images, icons, colors, fontSizes } from "../constants/index";
+
+function DoubleCommonButton(props) {
+  const { onPressLeft, titleLeft, onPressRight, titleRight } = props;
+  const { styleLeft, styleRight } = props;
+
+  let fontSizeTitleLeft = fontSizes.h4;
+  let fontSizeTitleRight = fontSizes.h4;
+
+  if (titleLeft.length > 6) {
+    fontSizeTitleLeft = fontSizes.h6;
+  }
+  if (titleRight.length > 6) {
+    fontSizeTitleRight = fontSizes.h6;
+  }
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={onPressLeft} style={[styles.buttons, styleLeft]}>
+        <Text
+          style={{
+            padding: 11,
+            fontSize: fontSizeTitleLeft,
+            fontWeight: "bold",
+            color: colors.PrimaryObjects,
+          }}
+        >
+          {titleLeft}
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={onPressRight} style={[styles.buttons, styleRight]}>
+        <Text
+          style={{
+            padding: 11,
+            fontSize: fontSizeTitleRight,
+            fontWeight: "bold",
+            color: colors.PrimaryObjects,
+          }}
+        >
+          {titleRight}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+export default DoubleCommonButton;
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 15,
+    paddingHorizontal: 25,
+    flexDirection: "row",
+    justifyContent: 'space-between',
+  },
+  buttons: {
+    width: '45%',
+    paddingHorizontal: 5,
+
+    borderColor: colors.PrimaryOnContainerAndFixed,
+    borderWidth: 1,
+    borderRadius: 30,
+
+    backgroundColor: colors.PrimaryBackground,
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
---
+++ src/components/index.js
@@ -0,0 +1,64 @@
+import CommonButton from "./CommonButton";
+import UIHeader from "./UIHeader";
+import EnterMessageBar from "./EnterMessageBar";
+import MessengerItems from "./MessengerItems";
+import DoubleCommonButton from "./DoubleCommonButton";
+
+import Icon from "./MyIcon";
+import {
+  RowSectionTitle,
+  RowSectionDisplay,
+  RowSectionNavigate,
+} from "./RowSection";
+import { SubInfoVertical, SubInfoHorizontal } from "./SubInfo";
+import { TextInputMediumIcon, TextInputTransparent } from "./MyInput";
+import { SubjectBox, ContentBox } from "./MySubjectAndContentBox";
+import { SearchBarAndButton, SearchBarTransparent } from "./MySearchBars";
+import { WhiteSlideBottomUp } from "./MyModal";
+import { FloatingButtonSingle, FloatingButtonDouble } from "./MyFloatingButton";
+
+import IconRating from "./IconRating";
+import { ReviewItems, NewReviewInput, ReviewFinalViewOnly } from "./Review";
+
+import Dropdown from "./MyDropdown";
+
+import { LoadingFullScreen } from "./MyLoadingScreen";
+
+///
+import { MiddleSingleMediumButton } from "./Buttons";
+
+
+export {
+  MiddleSingleMediumButton,
+  //
+  CommonButton,
+  UIHeader,
+  EnterMessageBar,
+  MessengerItems,
+  DoubleCommonButton,
+  //
+  Icon,
+  RowSectionTitle,
+  RowSectionDisplay,
+  RowSectionNavigate,
+  SubInfoVertical,
+  SubInfoHorizontal,
+  TextInputMediumIcon,
+  TextInputTransparent,
+  SubjectBox,
+  ContentBox,
+  SearchBarAndButton,
+  SearchBarTransparent,
+  WhiteSlideBottomUp,
+  FloatingButtonSingle,
+  FloatingButtonDouble,
+  //
+  IconRating,
+  ReviewItems,
+  NewReviewInput,
+  ReviewFinalViewOnly,
+  //
+  Dropdown,
+  //
+  LoadingFullScreen,
+};

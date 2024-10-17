9a6b2e9da1e7b2670c0e1e4291f955a5816fe92d
ADMIN <21522704@gm.uit.edu.vn>
2024-10-17 09:08:41 +0700

    add constants

---
+++ src/constants/colors.js
@@ -0,0 +1,41 @@
+export default {
+  PrimaryBackground: '#d0920b',
+  PrimaryObjects: '#FFFFFF',
+  PrimaryContainer: '#fdf3d3',
+  PrimaryOnContainerAndFixed: '#493104',
+
+  SecondaryBackground: '#9b5800',
+  SecondaryObjects: '#FFFFFF',
+  SecondaryContainer: '#ffe3c2',
+  SecondaryOnContainerAndFixed: '#352200',
+
+  TertiaryBackground: '#146C2E',
+  TertiaryObjects: '#FFFFFF',
+  TertiaryContainer: '#C4EED0',
+  TertiaryOnContainerAndFixed: '#072711',
+
+  RedBackground: '#6c1414',
+  RedLightBackground: '#d44141',
+  RedObjects: '#FFFFFF',
+  RedContainer: '#eec4c4',
+  RedOnContainerAndFixed: '#270707',
+
+  GrayBackground: '#707070',
+  GrayObjects: '#FFFFFF',
+  GrayContainer: '#eceeed',
+  GrayOnContainerAndFixed: '#2a2b2a',
+
+  backgroundWhite: "#fafaff",
+  transparentWhite: "rgba(250,250,250,0.8)",
+  noImportantText: "rgba(145, 160, 160, 0.75)",
+
+  inactive: "rgba(182, 205, 212, 0.75)",
+  active: "rgb(238, 88, 88)",
+
+  rating0: 'crimson',
+  rating1: 'red',
+  rating2: 'orange',
+  rating3: 'gold',
+  rating4: 'navy',
+  rating5: 'green',
+};
---
+++ src/constants/images.js
@@ -0,0 +1,10 @@
+const IMG_BASE_URL = '../assets/';
+
+export default {
+  background: require(IMG_BASE_URL + 'background.jpg'),
+  appLogo: require(IMG_BASE_URL + 'Logo.png'),
+
+  blankImageLoading: 'https://img.freepik.com/premium-vector/no-photos-icon-vector-image-can-be-used-spa_120816-264914.jpg',
+  blankAvatarForRegistration: "https://static.vecteezy.com/system/resources/previews/019/243/593/original/illustration-realistic-cute-blue-person-icon-3d-creative-isolated-on-background-vector.jpg",
+  blankAvatarForNewGroup: "https://img.freepik.com/premium-vector/audiences-icon-vector-image-can-be-used-public-relations-agency_120816-248194.jpg",
+};

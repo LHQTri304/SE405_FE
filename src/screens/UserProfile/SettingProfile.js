b0b805512c871710562d5f6376673d3289e4bf5d
ADMIN <21522704@gm.uit.edu.vn>
2024-10-17 09:29:03 +0700

    add user profile

---
+++ src/screens/UserProfile/SettingProfile.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import TabSettingBasicInfo from "./Tabs/TabSettingBasicInfo";
+import TabSettingTopics from "./Tabs/TabSettingTopics";
+import { images, icons, colors, fontSizes } from "../../constants";
+import { UIHeader } from "../../components";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+
+const Tab = createMaterialTopTabNavigator();
+
+const ScreenOptions = ({ route }) => ({
+  tabBarShowLabel: true,
+  tabBarActiveTintColor: colors.active,
+  tabBarInactiveTintColor: colors.inactive,
+  tabBarActiveBackgroundColor: colors.backgroundWhite,
+  tabBarInactiveBackgroundColor: colors.backgroundWhite,
+  tabBarLabelStyles:{
+    fontSize: fontSizes.h7,
+  }
+});
+
+export default function SettingProfile(props) {
+  const { navigate, goBack } = props.navigation;
+
+  return (
+    <View style={styles.container}>
+      <UIHeader
+        title={"Tùy chỉnh"}
+        leftIconName={icons.backIcon}
+        rightIconName={null}
+        onPressLeftIcon={() => {
+          navigate("MainBottomTab", { tabName: "UserProfile" })
+        }}
+        onPressRightIcon={() => {}}
+      />
+      <View style={styles.displayView}>
+        <Tab.Navigator
+          initialRouteName="TabSettingBasicInfo"
+          screenOptions={ScreenOptions}
+        >
+          <Tab.Screen
+            name="TabSettingBasicInfo"
+            component={TabSettingBasicInfo}
+            options={{
+              tabBarLabel: "Thông tin cơ bản",
+            }}
+          />
+          <Tab.Screen
+            name="TabSettingTopics"
+            component={TabSettingTopics}
+            options={{
+              tabBarLabel: "Chủ đề bạn quan tâm",
+            }}
+          />
+        </Tab.Navigator>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.backgroundWhite,
+  },
+  displayView: {
+    flex: 1,
+    flexDirection: "column",
+  },
+});
---
+++ src/screens/UserProfile/UserProfile.js
@@ -0,0 +1,285 @@
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
+import { images, icons, colors, fontSizes } from "../../constants";
+import {
+  UIHeader,
+  RowSectionTitle,
+  RowSectionDisplay,
+  RowSectionNavigate,
+} from "../../components";
+//import { randomGenerateColor } from "../../utilities";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+//import * as ImagePicker from "expo-image-picker";
+import {
+  profile_getUser,
+  profile_getAvatar,
+  profile_uploadImage,
+  user_getUser,
+  information_getAllFavoriteTopics,
+} from "../../api";
+
+export default function UserProfile(props) {
+  const [username, setUsername] = useState(null);
+  const [fulname, setFulName] = useState('Nguyễn Văn A');
+  const [image, setImage] = useState(null);
+
+  const [phoneNumber, setPhoneNumber] = useState('0908850624');
+  const [email, setEmail] = useState('21522704@gm.uit.edu.vn');
+  const [description, setDescription] = useState('Sinh viên năm 4');
+  const [yearOfBirth, setYearOfBirth] = useState('2003');
+  const [gender, setGender] = useState('Nam');
+
+  const [topics, setTopics] = useState([]);
+
+  const setEverything = (
+    username,
+    fulname,
+    email,
+    phoneNumber,
+    description,
+    yearOfBirth,
+    gender,
+    topics
+  ) => {
+    setUsername(username);
+    setFulName(fulname);
+    setTopics(topics);
+
+    description == null
+      ? setDescription("Chưa cập nhật")
+      : setDescription(description);
+
+    phoneNumber == 0
+      ? setPhoneNumber("Chưa cập nhật")
+      : setPhoneNumber("0" + phoneNumber);
+
+    email == null ? setEmail("Chưa cập nhật") : setEmail(email);
+
+    yearOfBirth <= 1900
+      ? setYearOfBirth("Chưa cập nhật")
+      : setYearOfBirth(yearOfBirth);
+
+    gender == null ? setGender("Chưa cập nhật") : setGender(gender);
+  };
+
+  useEffect(() => {/* 
+    const fetchData = async () => {
+      try {
+        const username = await AsyncStorage.getItem("username");
+        const responseUser = await user_getUser();
+        const responseFavTopics = await information_getAllFavoriteTopics(
+          responseUser.information.infoID
+        );
+
+        let topicNames = responseFavTopics.map((item) => item.topicName);
+        //console.log(topicNames);
+
+        setEverything(
+          username,
+          responseUser.information.fulName,
+          responseUser.email,
+          responseUser.information.phoneNumber,
+          responseUser.information.description,
+          responseUser.information.yearOfBirth,
+          responseUser.information.gender,
+          topicNames
+        );
+
+        const responseAvatar = await profile_getAvatar(username);
+        if (responseAvatar.data != null) {
+          setImage(responseAvatar.data.toString());
+        }
+        //console.log(image);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+
+    const intervalId = setInterval(fetchData, 3000);
+    return () => clearInterval(intervalId); */
+  }, [props.userName]);
+
+  /* useEffect(() => {
+    (async () => {
+      const { status } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== "granted") {
+        alert("Sorry, we need camera roll permissions to make this work!");
+      }
+    })();
+  }, []); */
+
+  const Logout = async () => {
+    try {
+      //await AsyncStorage.removeItem("username");
+      navigate("Login");
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
+
+  const selectImage = async () => {/* 
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+
+      try {
+        var imagePath = result.assets[0].uri.toString();
+        await profile_uploadImage(imagePath, username);
+      } catch (error) {
+        console.error("Error uploading image:", error);
+      }
+    } */
+  };
+
+  /* const ShowPicture = () => {
+    navigate("ShowPicture", { file: image });
+  }; */
+
+  //function of navigation to/back
+  const { navigate, goBack } = props.navigation;
+
+  return (
+    <View style={styles.container}>
+      <UIHeader title={"Hồ sơ"} />
+
+      <ScrollView>
+        <View style={styles.profileView}>
+        <Image source={images.background} style={styles.profileImage} />
+        <Text style={styles.profileUsername}>{fulname}</Text>
+            <Text style={styles.profileDescription} numberOfLines={4}>
+              {description}
+            </Text>
+        </View>
+
+        {/* <RowSectionTitle text={"Chủ đề yêu thích"} styles={{ marginTop: 20 }} /> */}
+
+        {/* <View style={styles.topics_container}>
+          {topics.map((topicName, index) => (
+            <View
+              style={[
+                styles.eachTopicBox,
+                { borderColor: 'black' },
+              ]}
+              key={index}
+            >
+              <Text style={styles.eachTopicBoxText}>{topicName}</Text>
+            </View>
+          ))}
+        </View> */}
+
+        <RowSectionTitle
+          text={"Thông tin tài khoản"}
+          styles={{ marginTop: 20 }}
+        />
+
+        <RowSectionDisplay icon={icons.phoneIcon} text={phoneNumber} />
+        <RowSectionDisplay icon={icons.emailIcon} text={email} />
+        {/* <RowSectionDisplay icon={icons.birthdayCakeIcon} text={yearOfBirth} /> */}
+        {/* <RowSectionDisplay icon={icons.genderEqualityIcon} text={gender} /> */}
+
+        {/* <RowSectionTitle text={"Tùy chỉnh tài khoản"} /> */}
+
+        {/* <RowSectionNavigate
+          icon={icons.personIcon}
+          text={"Đổi thông tin cá nhân"}
+          onPress={() => navigate("SettingProfile")}
+        /> */}
+
+        <RowSectionNavigate
+          icon={icons.keyIcon}
+          text={"Đổi mật khẩu"}
+          onPress={() => navigate("ResetPassword")}
+        />
+        <RowSectionNavigate
+          icon={icons.exportIcon}
+          text={"Đăng xuất"}
+          onPress={Logout}
+        />
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.backgroundWhite,
+  },
+  profileView: {
+    marginLeft: "5%",
+    marginVertical: 15,
+    alignItems: "center",
+    //flexDirection: "row",
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    resizeMode: "cover",
+    marginHorizontal: 15,
+    marginVertical: 5,
+    borderRadius: 90,
+    borderColor: colors.PrimaryBackground,
+    borderWidth: 2,
+  },
+  profileUsername: {
+    color: "black",
+    fontSize: fontSizes.h5,
+    fontWeight: "900",
+  },
+  profileDescription: {
+    height: 70,
+    maxWidth: 220,
+    color: "gray",
+    fontSize: fontSizes.h7,
+  },
+  button: {
+    backgroundColor: colors.PrimaryBackground,
+    padding: 3,
+    borderRadius: 10,
+    width: 90,
+    alignItems: "center",
+    alignSelf: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: fontSizes.h8,
+  },
+  //
+  topics_container: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+  },
+  eachTopicBox: {
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    marginBottom: 5,
+    marginHorizontal: 2,
+    borderRadius: 12,
+    borderWidth: 3,
+    borderColor: colors.GrayContainer,
+    backgroundColor: colors.GrayObjects,
+  },
+  eachTopicBoxText: {
+    color: colors.GrayOnContainerAndFixed,
+    textAlign: "center",
+    fontSize: fontSizes.h7,
+  },
+});

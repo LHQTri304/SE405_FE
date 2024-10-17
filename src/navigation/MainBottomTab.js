d4acfd00269491fdaa485859242760326e2f43db
ADMIN <21522704@gm.uit.edu.vn>
2024-10-17 09:06:56 +0700

    add navigation

---
+++ src/navigation/MainBottomTab.js
@@ -0,0 +1,103 @@
+import * as React from "react";
+import { View, Alert } from "react-native";
+import {
+  UserProfile,
+  GroupChat,
+  Friends,
+  AllNotification,
+  ChatbotScreen,
+} from "../screens";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { images, icons, colors, fontSizes } from "../constants";
+import { Icon } from "../components";
+
+const Tab = createBottomTabNavigator();
+
+const ScreenOptions = ({ route }) => ({
+  headerShown: false,
+  tabBarActiveTintColor: colors.active,
+  tabBarInactiveTintColor: colors.inactive,
+  tabBarActiveBackgroundColor: colors.backgroundWhite,
+  tabBarInactiveBackgroundColor: colors.backgroundWhite,
+
+  tabBarIcon: ({ focused, color, size }) => {
+    let screenName = route.name;
+    let iconName = icons.personIcon;
+    if (screenName == "GroupChat") {
+      iconName = icons.groupIcon;
+    } else if (screenName == "Friends") {
+      iconName = icons.activeChatMessageIcon;
+    } else if (screenName == "Notifications") {
+      iconName = icons.activeBellAlarm;
+    } else if (screenName == "MessageBot") {
+      iconName = icons.activeFAQIcon;
+    }
+
+    return (
+      <Icon
+        name={iconName}
+        size={focused ? 30 : 20}
+        color={color}
+        style={{ marginTop: "10%" }}
+      />
+    );
+  },
+});
+
+const tabBarLabelStyles = {
+  fontSize: fontSizes.h7,
+  marginTop: "5%",
+  marginBottom: "5%",
+};
+
+export default function MainBottomTab(props) {
+  const { tabName } = props.route.params;
+
+  return (
+    <Tab.Navigator
+      initialRouteName={tabName == null ? "UserProfile" : tabName}
+      screenOptions={ScreenOptions}
+    >
+      <Tab.Screen
+        name="Friends"
+        component={Friends}
+        options={{
+          //tabBarLabel: "Bạn bè",
+          tabBarLabelStyle: tabBarLabelStyles,
+        }}
+      />
+      <Tab.Screen
+        name="GroupChat"
+        component={GroupChat}
+        options={{
+          //tabBarLabel: "Nhóm",
+          tabBarLabelStyle: tabBarLabelStyles,
+        }}
+      />
+      <Tab.Screen
+        name="MessageBot"
+        component={ChatbotScreen}
+        options={{
+          //tabBarLabel: "Hỏi đáp",
+          tabBarLabelStyle: tabBarLabelStyles,
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={AllNotification}
+        options={{
+          //tabBarLabel: "Thông báo",
+          tabBarLabelStyle: tabBarLabelStyles,
+        }}
+      />
+      <Tab.Screen
+        name="UserProfile"
+        component={UserProfile}
+        options={{
+          //tabBarLabel: "Tài khoản",
+          tabBarLabelStyle: tabBarLabelStyles,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
---
+++ src/navigation/stackNavigator.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {
+  Login,
+  Registration,
+  ForgetPassword,
+  Verification,
+  ResetPassword,
+  UserProfile,
+} from '../screens';
+//import MainBottomTab from './MainBottomTab';
+
+const Stack = createNativeStackNavigator();
+
+export default function StackNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Registration" component={Registration} />
+        <Stack.Screen name="ForgetPassword" component={ForgetPassword} />
+        <Stack.Screen name="Verification" component={Verification} />
+        <Stack.Screen name="ResetPassword" component={ResetPassword} />
+
+        <Stack.Screen name="UserProfile" component={UserProfile} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}

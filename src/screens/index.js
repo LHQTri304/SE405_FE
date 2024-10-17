58cec5af7032c0b86b12a84ae2b5b94a61524c90
ADMIN <21522704@gm.uit.edu.vn>
2024-10-17 09:09:16 +0700

    add auth screen

---
+++ src/screens/index.js
@@ -0,0 +1,16 @@
+import Login from "./Auth/Login";
+import Registration from "./Auth/Registration";
+import ForgetPassword from "./Auth/ForgetPassword";
+import Verification from "./Auth/Verification";
+import ResetPassword from "./Auth/ResetPassword";
+
+import UserProfile from "./UserProfile/UserProfile";
+
+export {
+  Login,
+  Registration,
+  ForgetPassword,
+  Verification,
+  ResetPassword,
+  UserProfile,
+};
